b68454a6683af765fc82cafae32a4ef6608e0e83
G. Eli Jergensen <gelijergensen@ou.edu>
2018-09-06 14:03:13 -0500

    Added more test cases for LinearKernel getMatrix

--- test/testUtils.py
+++ test/testUtils.py
@@ -1,7 +1,15 @@
 """A quick utility function which prints a statement if the expected result does not match the actual result"""
 
+import numpy as np
+
 def testFunction(statement, expected_result, actual_result):
-    if expected_result != actual_result:
-        print(statement)
-        print("Expected:", expected_result)
-        print("Acutal:", actual_result)
+    if isinstance(expected_result, np.ndarray):
+        if not np.allclose(expected_result, actual_result):
+            print(statement)
+            print("Expected:", expected_result)
+            print("Acutal:", actual_result)
+    else:
+        if expected_result != actual_result:
+            print(statement)
+            print("Expected:", expected_result)
+            print("Acutal:", actual_result)
